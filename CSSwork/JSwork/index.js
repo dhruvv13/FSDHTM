30b58dddeb356a2d07337c43b2f63f311cfcaf90
dhruvv13 <dhruvgupta3724@gmail.com>
2024-10-18 13:09:51 +0530

    amazonn

---
+++ CSSwork/JSwork/index.js
@@ -0,0 +1,12 @@
+// console.log("hello world");
+
+// var a =12;
+// if(a>10){
+//     b=20;
+//     console.log("a inside:"+a);
+// }
+// console.log("a outside:"+b);
+function fun(a,b){
+    console.log("hiiii");
+}
+fun();
