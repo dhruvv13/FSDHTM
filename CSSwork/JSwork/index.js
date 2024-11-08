a95f918747f6b92a0b67c4267d71f69e1a467014
dhruvv13 <dhruvgupta3724@gmail.com>
2024-11-08 11:44:28 +0530

    js work

--- CSSwork/JSwork/index.js
+++ CSSwork/JSwork/index.js
@@ -6,7 +6,33 @@
 //     console.log("a inside:"+a);
 // }
 // console.log("a outside:"+b);
-function fun(a,b){
-    console.log("hiiii");
+//console.log("hi, inside index.js");
+//function fun(a,b){
+  //  console.log("hiiii");
+    //return a+b;
+//}
+//const result = fun(23,12);
+//console.log(result);
+const button = document.getElementById("btn");
+console.dir(button);
+
+function selectlanguage(language){
+    console.log("Hi, Inside Language")
+    let data;
+    if(language=='c'){
+function cCompiler()
+{
+    return "C compiler"
 }
-fun();
+data=cCompiler()
+    }
+    if(language=='java'){  
+function javacompiler()
+{
+    return "java compiler"
+}
+data = javacompiler()
+    }
+    return data;
+}
+console.log(selectlanguage("java"));
